cc37fd9f88369a4b9f2254a2882b0d1462ddc97e
Hlaing Min Than(Faizal) <hlaingminthan92@gmail.com>
2021-06-27 12:22:29 +0630

    recaptcha reference more flexible

---
+++ config/council.php
@@ -0,0 +1,8 @@
+<?php
+
+return [
+    'recaptcha' => [
+        'sercret_key' => env('RECAPTCHA_SECRET_KEY'),
+        'site_key' => env('RECAPTCHA_SITE_KEY'),
+    ]
+];
--- resources/views/threads/create.blade.php
+++ resources/views/threads/create.blade.php
@@ -1,5 +1,9 @@
 @section('head')
-<script src="https://www.google.com/recaptcha/api.js" async defer></script>
+<script
+    src="https://www.google.com/recaptcha/api.js"
+    async
+    defer
+></script>
 @endsection
 <x-app-layout>
     <x-slot name="header">
@@ -12,42 +16,86 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 bg-white border-b border-gray-200">
-                    <form action="{{route('threads.store')}}" method="POST" id="form" class="mt-3">
-                            @csrf
-                            @error("title")
-                            <p class="text-base  text-red-400" id="result">{{$message}}</p>
-                            @enderror
-                            <div class="mb-6">
-                                <input name="title" value="{{old('title')}}" placeholder="Your Thread Title" class="w-full px-3 py-2 placeholder-gray-300 border border-gray-300 rounded-md focus:outline-none focus:ring focus:ring-indigo-100 focus:border-indigo-300 dark:bg-gray-700 dark:text-white dark:placeholder-gray-500 dark:border-gray-600 dark:focus:ring-gray-900 dark:focus:border-gray-500" ></input>
-                            </div>
-                            @error("body")
-                            <p class="text-base  text-red-400" id="result">{{$message}}</p>
-                            @enderror
-                            <div class="mb-6">
-                                <textarea class="createThread" rows="5" name="body" id="message" placeholder="Your body" class="w-full px-3 py-2 placeholder-gray-300 border border-gray-300 rounded-md focus:outline-none focus:ring focus:ring-indigo-100 focus:border-indigo-300 dark:bg-gray-700 dark:text-white dark:placeholder-gray-500 dark:border-gray-600 dark:focus:ring-gray-900 dark:focus:border-gray-500" >{{old('body')}}</textarea>
-                            </div>
-                            <div class="mb-6">
-                                <select  name="channel_id" id="message" class="w-full px-3 py-2 placeholder-gray-300 border border-gray-300 rounded-md focus:outline-none focus:ring focus:ring-indigo-100 focus:border-indigo-300 dark:bg-gray-700 dark:text-white dark:placeholder-gray-500 dark:border-gray-600 dark:focus:ring-gray-900 dark:focus:border-gray-500" >
-                                        <option value="">Choose One...</option>
-                                    @foreach($channels as $channel)
-                                        <option value="{{$channel->id}}" {{old('channel_id')==$channel->id ?'selected':''}}>{{$channel->name}}</option>
-                                    @endforeach
-                                </select>
-                            </div>
-                            @error("channel_id")
-                            <p class="text-base  text-red-400" id="result">{{$message}}</p>
-                            @enderror
-                            <div class="mb-6">
-                                <div class="g-recaptcha" data-sitekey="6Le4F5kaAAAAAG4Vy7K_yQLDzeUkJvGS1izSLztF"></div>
-                            </div>
-                            @error('g-recaptcha-response')
-                            <p class="text-base  text-red-400" id="result">{{$message}}</p>
-                            @enderror
-                            <div class="mb-6">
-                                <button  class="w-full px-3 py-4 text-white bg-blue-500 rounded-md focus:bg-blue-600 focus:outline-none">Add Thread</button>
-                            </div>
-                            <p class="text-base text-center text-gray-400" id="result">
-                            </p>
+                    <form
+                        action="{{route('threads.store')}}"
+                        method="POST"
+                        id="form"
+                        class="mt-3"
+                    >
+                        @csrf
+                        @error("title")
+                        <p
+                            class="text-base  text-red-400"
+                            id="result"
+                        >{{$message}}</p>
+                        @enderror
+                        <div class="mb-6">
+                            <input
+                                name="title"
+                                value="{{old('title')}}"
+                                placeholder="Your Thread Title"
+                                class="w-full px-3 py-2 placeholder-gray-300 border border-gray-300 rounded-md focus:outline-none focus:ring focus:ring-indigo-100 focus:border-indigo-300 dark:bg-gray-700 dark:text-white dark:placeholder-gray-500 dark:border-gray-600 dark:focus:ring-gray-900 dark:focus:border-gray-500"
+                            ></input>
+                        </div>
+                        @error("body")
+                        <p
+                            class="text-base  text-red-400"
+                            id="result"
+                        >{{$message}}</p>
+                        @enderror
+                        <div class="mb-6">
+                            <textarea
+                                class="createThread"
+                                rows="5"
+                                name="body"
+                                id="message"
+                                placeholder="Your body"
+                                class="w-full px-3 py-2 placeholder-gray-300 border border-gray-300 rounded-md focus:outline-none focus:ring focus:ring-indigo-100 focus:border-indigo-300 dark:bg-gray-700 dark:text-white dark:placeholder-gray-500 dark:border-gray-600 dark:focus:ring-gray-900 dark:focus:border-gray-500"
+                            >{{old('body')}}</textarea>
+                        </div>
+                        <div class="mb-6">
+                            <select
+                                name="channel_id"
+                                id="message"
+                                class="w-full px-3 py-2 placeholder-gray-300 border border-gray-300 rounded-md focus:outline-none focus:ring focus:ring-indigo-100 focus:border-indigo-300 dark:bg-gray-700 dark:text-white dark:placeholder-gray-500 dark:border-gray-600 dark:focus:ring-gray-900 dark:focus:border-gray-500"
+                            >
+                                <option value="">Choose One...</option>
+                                @foreach($channels as $channel)
+                                <option
+                                    value="{{$channel->id}}"
+                                    {{old('channel_id')==$channel->id ?'selected':''}}
+                                >{{$channel->name}}</option>
+                                @endforeach
+                            </select>
+                        </div>
+                        @error("channel_id")
+                        <p
+                            class="text-base  text-red-400"
+                            id="result"
+                        >{{$message}}</p>
+                        @enderror
+                        <div class="mb-6">
+                            <div
+                                class="g-recaptcha"
+                                data-sitekey="{{config('council.recaptcha.site_key')}}"
+                            ></div>
+                        </div>
+                        @error('g-recaptcha-response')
+                        <p
+                            class="text-base  text-red-400"
+                            id="result"
+                        >{{$message}}</p>
+                        @enderror
+                        <div class="mb-6">
+                            <button
+                                class="w-full px-3 py-4 text-white bg-blue-500 rounded-md focus:bg-blue-600 focus:outline-none"
+                            >Add Thread</button>
+                        </div>
+                        <p
+                            class="text-base text-center text-gray-400"
+                            id="result"
+                        >
+                        </p>
                     </form>
                 </div>
             </div>
